cebbf4d96ebf3b40f9a5d4e294442c5870cf3ddd
AshrafSaleem <ses160001@student.iumw.edu.my>
2019-02-23 15:59:30 +0800

    Second Commit
    Write data to text file eveyrtime -i is infront of a input.

--- main.cpp
+++ main.cpp
@@ -2,24 +2,29 @@
 #include <fstream>
 #include <string.h>
 
-const char* fileName = "list.txt";
+const char *fileName = "list.txt";
 
 int main(
-        int argc,           // Argument Counter : Number of arguments, default is 1 (first arguments are always the filename itself) 
-        char const *argv[]  // Argument Value : The value of arguments
-        )
+    int argc,          // Argument Counter : Number of arguments, default is 1 (first arguments are always the filename itself)
+    char const *argv[] // Argument Value : The value of arguments
+)
 {
     if (argc > 1)
+    {
+        int i = 0;
+        while (i < argc)
         {
-            if (strcmp(argv[1], "-i\n"))
-            { 
-                std::fstream ff; 
-                ff.open(fileName,std::ios::out);
-                ff << argv[2] << std::endl;   
-                ff.close();  
-                return 0; 
+            if ((strcmp(argv[i], "-i") == 0) && (i+1) != argc) 
+            {
+                std::fstream ff;
+                ff.open(fileName, std::ios::out|std::ios::app);
+                ff << argv[i+1] << std::endl;
+                ff.close();
             }
-        } 
 
-   return 0;
+            i++;
+        }
+    }
+
+    return 0;
 }
